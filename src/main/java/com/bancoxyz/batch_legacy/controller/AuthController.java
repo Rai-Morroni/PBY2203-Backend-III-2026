8f49ba5ccda0e9ca1a362cd22de50c50e79c9c24
Rainiero Morroni <ra.morroni@duocuc.cl>
2026-09-14 21:56:09 -0300

    feat: Se implementan dependencias JWT, JwtUtil y AuthController para login

---
+++ src/main/java/com/bancoxyz/batch_legacy/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.bancoxyz.batch_legacy.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bancoxyz.batch_legacy.config.JwtUtil;
+import com.bancoxyz.batch_legacy.model.LoginRequest;
+
+@RestController
+@RequestMapping("/api/auth") // Ruta base para la autenticación. Único endpoint público que no requerirá token, ya que su trabajo es generarlo.
+public class AuthController {
+
+    @Autowired private JwtUtil jwtUtil;
+    @Autowired private UserDetailsService userDetailsService;
+    @Autowired private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login") // Endpoint para el login
+    public Map<String, String> login(@RequestBody LoginRequest request) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.username());
+        
+        if (passwordEncoder.matches(request.password(), userDetails.getPassword())) {
+            String token = jwtUtil.generateToken(userDetails);
+            return Map.of("token", token);
+        }
+        throw new RuntimeException("Credenciales inválidas");
+    }
+}
---
+++ src/main/java/com/bancoxyz/batch_legacy/model/LoginRequest.java
@@ -0,0 +1,3 @@
+package com.bancoxyz.batch_legacy.model;
+
+public record LoginRequest(String username, String password) {}
